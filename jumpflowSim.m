5eb05c995d59f6a8a08665ef9a8e61276f1c4153
LuukSpin <56606436+LuukSpin@users.noreply.github.com>
2021-11-17 09:46:36 +0100

    Changed jumpflowSim a little bit

--- jumpflowSim.m
+++ jumpflowSim.m
@@ -1,9 +1,9 @@
-function [t, zc,k, zd] = jumpflowSim(objFJ, wc, wd, h, opts)
+function [t, zc,k, zd] = jumpflowSim(objJF, wc, wd, h, opts)
 %wc and wd should be function handles, otherwise the HyEQsolver cannot
 %produce an output.
 
 arguments
-    objFJ   (1,1) JumpFlowSystem
+    objJF   (1,1) JumpFlowSystem
     wc
     wd
     h       (1,1) double
@@ -18,12 +18,12 @@ TSPAN = [0 opts.simulation.Tend]; % simulation time interval
 JSPAN = [0 ceil(TSPAN(2)/h)]; % simulation interval for discrete jumps
 
 % Flow matrices
-Ac = objFJ.Ac;
-Bc = objFJ.Bwc;
+Ac = objJF.Ac;
+Bc = objJF.Bwc;
 
 % Jump matrices
-Ad = objFJ.Ad;
-Bd = objFJ.Bwd;
+Ad = objJF.Ad;
+Bd = objJF.Bwd;
 
 x0 = zeros(size(Ac, 1)+1, 1);
 [t, ~, x] = HyEQsolver(@(x,t) f(x,wc(t), Ac, Bc), @(x,t) g(x,Ad,Bd,wd(t)), @(x) flowset(x,h), @(x) jumpset(x,h), x0, TSPAN, JSPAN, opts.simulation.rule, opts.simulation.options);
@@ -40,13 +40,13 @@ for i = 1:length(t)
 end
 
 % Continuous-time performance channels
-Cc = objFJ.Czc;
-Dc = objFJ.Dzc_wc;
+Cc = objJF.Czc;
+Dc = objJF.Dzc_wc;
 zc = Cc*xi'+Dc*w_c;
 
 % Discrete-time performance channels
-Cd = objFJ.Czd;
-Dd = objFJ.Dzd_wd;
+Cd = objJF.Czd;
+Dd = objJF.Dzd_wd;
 zd = Cd*xi(ind, :)'+Dd*w_d(:, ind);
 
 end
